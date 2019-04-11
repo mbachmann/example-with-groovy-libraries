9f7d20be3c29f759eab016aff7fc0925f15c8652
Becirbegovic Edin (IT-SWE-CC3-TS3) <edin.becirbegovic@sbb.ch>
2019-04-11 11:28:20 +0200

    try mis script with deff

--- vars/getReleasedVersion.groovy
+++ vars/getReleasedVersion.groovy
@@ -1,6 +1,23 @@
 def call() {
 
     println "Hallo das script chani lässe"
-    readFile 'build.gradle'
+    //readFile 'build.gradle'
+
+
+        POM = sh ( script: 'cat build.gradle', returnStdout: true)
+        def list = readGradleBuild file: 'build.gradle'
+
+        assert list instanceof groovy.util.Node
+        assert list.technology.name.text() == 'Groovy'
+
+        echo " Halloi ha hallo"
+        //println "Step Read POM"
+        return POM;
+    
+    return this
+
+
+
+
     //(readFile('build.gradle') =~ '<version>(.+)-SNAPSHOT</version>')[0][1]
 }
